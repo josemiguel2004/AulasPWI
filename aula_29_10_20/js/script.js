a4aea55df56a12bf5cec22c210e4c84826d8027d
Miguel Jose Albano Da Silva <miguel.zico@outlook.com>
2020-10-28 15:46:14 -0300

    AULA 28/10/2020
    ATIVIDADES COM JAVASCRIPT PARTE1

---
+++ aula_29_10_20/js/script.js
@@ -0,0 +1,56 @@
+function escreveola(){
+    document.write("<h3>Olá bem-vindo ao meu site</h3>")
+}
+
+var tag= document.getElementById("titulo")
+function mudacortitulo(){
+    tag.style.color= "#4682B4";
+
+    
+}
+
+var tag= document.getElementById("titulo")
+function mudacortitulo2(){
+    tag.style.color="#40E0D0";
+}
+
+var tag= document.getElementById("titulo")
+function mudacortitulo3(){
+    tag.style.color="#006400";
+}
+
+var tag= document.getElementById("titulo")
+function mudacortitulo4(){
+    tag.style.color="#00FF00";
+}
+
+var tag= document.getElementById("titulo")
+function mudacortitulo5(){
+    tag.style.color="#D2691E";
+}
+
+var tag= document.getElementById("pagina")
+function mudacorpagina() {
+    tag.style.background="#9400D3";
+}
+
+var tag= document.getElementById("pagina")
+function mudacorpagina2() {
+    tag.style.background="#F08080";
+}
+
+var tag= document.getElementById("pagina")
+function mudacorpagina3() {
+    tag.style.background="#FF0000";
+}
+
+var tag= document.getElementById("pagina")
+function mudacorpagina4() {
+    tag.style.background="#FF4500";
+}
+
+var tag= document.getElementById("pagina")
+function mudacorpagina5() {
+    tag.style.background="#B0E0E6";
+}
+
